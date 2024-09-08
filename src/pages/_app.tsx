995be9b4d21b5cf53d18f550f44ef63935a6d92a
Rashmi-278 <rashmiabbigeri@gmail.com>
2024-09-08 16:51:07 +0530

    add chakra ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,20 +5,23 @@ import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
-
+import { ChakraProvider } from '@chakra-ui/react'
 import { config } from '../wagmi';
 
 const client = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={client}>
-        <RainbowKitProvider>
-          <Component {...pageProps} />
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+    <ChakraProvider>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={client}>
+          <RainbowKitProvider>
+            <Component {...pageProps} />
+          </RainbowKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </ChakraProvider>
+
   );
 }
 
